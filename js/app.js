b959f5ec560a810e019f92528209b830848b64ef
Owner <abcdot6661@gmail.com>
2022-09-03 01:38:22 -0700

    Add Pages

--- js/app.js
+++ js/app.js
@@ -105,9 +105,11 @@ window.addEventListener("DOMContentLoaded", () => {
 	// Form Validation
 
 	(() => {
-
+		'use strict'
 		// Fetch all the forms we want to apply custom Bootstrap validation styles to
 		const forms = document.querySelectorAll('.needs-validation')
+
+		console.log(forms)
 
 		// Loop over them and prevent submission
 		Array.from(forms).forEach(form => {
